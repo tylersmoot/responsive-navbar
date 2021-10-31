d1b181d9a5282819fab59ce11af077cbeb6d683c
Tyler <tylercsmoot8@gmail.com>
2021-10-30 21:58:19 -0500

    README.md

---
+++ script.js
@@ -0,0 +1,17 @@
+let dropBtn = document.getElementById("button");
+let dropContainer = document.querySelector(".dropdown-container");
+const dropLinks = document.getElementsByClassName("dropdown-container ul li a");
+
+
+
+dropBtn.addEventListener("click", function () {
+    
+    dropContainer.classList.toggle("dropdown-container-show");
+    
+
+});
+
+
+
+
+
